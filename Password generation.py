c3a19096118ec37b79def94a92fdaec1fc4c8a34
Lucifer-is-my-pet <dianka-piratka@list.ru>
2015-06-11 01:52:11 +0500

    Слияние коды с генератором и окном объединены, в связи с чем начато изменение методов класса генерации

--- Password generation.py
+++ Password generation.py
@@ -1,6 +1,8 @@
 __author__ = 'Гуральник'
 import random
 import re
+from PyQt4 import QtGui, QtCore
+import sys
 
 
 def read_file(fname):  # выкачиваем один раз в main и обращаемся уже к массиву
@@ -29,7 +31,7 @@ class Password:
         self.max = -1
         self.withDigits = False
         self.withSymbols = False
-        self.symbolsToUse = []
+        self.symbolsToUse = str()
         self.proverbs = read_file("proverbs.txt")
         self.numOfLines = len(self.proverbs)
         self.thatOneLine = -1  # номер строки, откуда получили пословицу
@@ -37,6 +39,9 @@ class Password:
 
     def __len__(self):
         return len(self.translit)
+
+    def __str__(self):
+        return self.translit
 
     def find_new(self):  # заполни-метод
         """
@@ -97,14 +102,14 @@ class Password:
             if letter == '\n':
                 break
             temp += self.dictionary[letter.lower()]
-        if not len(self):
-            self.translit = temp
 
         result = str()
         for i in temp:
-            if i in self.symbolsToUse or i.isalpha() or i == ' ':
+            if i in self.symbolsToUse or i.isalpha():
                 result += i
-        return result
+        if not len(self):
+            self.translit = result
+        # return result
 
     def number_of_spaces(self):
         """
@@ -168,6 +173,7 @@ class Password:
         в обрезанном - первую, последнюю и примерно в середине
         :return: str
         """
+        print("UP")
         if self.max == -1:
             splitted = self.translit.split(' ')
             temp = list()
@@ -187,9 +193,10 @@ class Password:
             result = self.translit[0].upper() + self.translit[1:-1] + self.translit[-1].upper()
             result = result[:len(result) // 2] + result[len(result) // 2].upper() + result[(len(result) // 2) + 1:]
 
-        return result
+        # return result
+        self.translit = result
 
-    def add_symbols(self, string, num):  # преобразуй-метод TODO
+    def add_symbols(self):  # преобразуй-метод TODO
         """
         :num: int, номер группы, если пользователь выбрал из предложенных, а не указал конкретные. 0 - не указал
         (проверять полную пословицу на наличие символов и соответствие их допустимому набору
@@ -199,65 +206,128 @@ class Password:
         """
         pass
 
-    def clear(self):
-        """
-        сбрасывает часть настроек после повторного нажатия "Сгенерировать"
-        :return: None
-        """
-        self.cyrillic = str()
-        self.translit = str()
-        self.min = -1
-        self.max = -1
-        self.withDigits = False
-        self.withSymbols = False
-        self.symbolsToUse = []
-        self.thatOneLine = -1
+
+class Window(QtGui.QWidget):
+    def __init__(self, passToGenerate, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.myPreciousPassword = passToGenerate
+        self.setGeometry(600, 300, 450, 300)
+        self.setWindowTitle("Генерация пароля")
+        self.grid = QtGui.QGridLayout()
+
+        self.generate = QtGui.QPushButton("Сгенерировать", self)
+        self.generate.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.connect(self.generate, QtCore.SIGNAL('clicked()'), self.prnt)
+        self.setFocus()
+
+        self.titlePrompting = QtGui.QLabel("Подсказка:")
+        self.labelPrompting = QtGui.QLabel("")  # setText()
+        self.labelPrompting.setAlignment(QtCore.Qt.AlignHCenter)
+        self.labelPrompting.setStyleSheet('background-color: #FFFAFA; font-size: 14px;')
+        self.titlePassword = QtGui.QLabel("Пароль:")
+        self.labelPassword = QtGui.QLabel("")
+        self.labelPassword.setAlignment(QtCore.Qt.AlignHCenter)
+        self.labelPassword.setStyleSheet('background-color: #FFFAFA; font-size: 14px;')
+
+        self.checkUp = QtGui.QCheckBox()
+        self.titleUp = QtGui.QLabel("с заглавными буквами")
+        self.checkDigits = QtGui.QCheckBox()
+        self.titleDigits = QtGui.QLabel("с цифрами")
+        self.checkSymbols = QtGui.QCheckBox()
+        self.titleSymbols = QtGui.QLabel("с символами:")
+        self.titleSymbols.setToolTip('Введите символы, не разделяя их запятой или пробелом')
+        self.inputSymbols = QtGui.QLineEdit(self)
+        self.inputSymbols.setToolTip('Введите символы, не разделяя их запятой или пробелом')
+        self.checkMin = QtGui.QCheckBox()
+        self.titleMin = QtGui.QLabel("минимальная длина:")
+        self.inputMin = QtGui.QLineEdit(self)
+        self.textMin = QtGui.QLabel("символов")
+        self.checkMax = QtGui.QCheckBox()
+        self.titleMax = QtGui.QLabel("максимальная длина:")
+        self.inputMax = QtGui.QLineEdit(self)
+        self.textMax = QtGui.QLabel("символов")
+
+        self.grid.addWidget(self.titlePrompting, 0, 1)
+        self.grid.addWidget(self.labelPrompting, 1, 0, 1, 4)
+        self.grid.addWidget(self.titlePassword, 2, 1)
+        self.grid.addWidget(self.labelPassword, 3, 0, 1, 4)
+
+        self.grid.addWidget(self.checkUp, 4, 0)
+        self.grid.addWidget(self.titleUp, 4, 1)
+        self.grid.addWidget(self.checkDigits, 5, 0)
+        self.grid.addWidget(self.titleDigits, 5, 1)
+        self.grid.addWidget(self.checkSymbols, 6, 0)
+        self.grid.addWidget(self.titleSymbols, 6, 1)
+        self.grid.addWidget(self.inputSymbols, 6, 2)
+        self.grid.addWidget(self.checkMin, 7, 0)
+        self.grid.addWidget(self.titleMin, 7, 1)
+        self.grid.addWidget(self.inputMin, 7, 2)
+        self.grid.addWidget(self.textMin, 7, 3)
+        self.grid.addWidget(self.checkMax, 8, 0)
+        self.grid.addWidget(self.titleMax, 8, 1)
+        self.grid.addWidget(self.inputMax, 8, 2)
+        self.grid.addWidget(self.textMax, 8, 3)
+
+        self.grid.addWidget(self.generate, 9, 1, 1, 2)
+        self.setLayout(self.grid)
+
+    def prnt(self):
+        # проверить чекбоксы и поля
+        if (not self.checkMin.isChecked() and self.inputMin.text()) or\
+                (not self.checkMax.isChecked() and self.inputMax.text()):
+            QtGui.QMessageBox.critical(self, 'Ошибка', "Не отмечено, что в пароле есть ограничение на длину")
+            return
+        if not self.checkSymbols.isChecked() and self.inputSymbols.text():
+            QtGui.QMessageBox.critical(self, 'Ошибка', "Не отмечено, что в пароле должны содержаться спец. символы")
+            return
+        self.myPreciousPassword.withDigits = self.checkDigits.isChecked()
+        self.myPreciousPassword.withSymbols = self.checkSymbols.isChecked()
+        if self.myPreciousPassword.withSymbols:
+            self.myPreciousPassword.symbolsToUse = self.inputSymbols.text().split(' ')
+        if self.checkMin.isChecked():
+            self.myPreciousPassword.min = self.inputMin.text()
+        if self.checkMax.isChecked():
+            self.myPreciousPassword.max = self.inputMax.text()
+
+        cyrillic = self.myPreciousPassword.find_new()
+        self.labelPrompting.setText(cyrillic)
+        if self.myPreciousPassword.translit:
+            self.myPreciousPassword.translit = str()
+        self.myPreciousPassword.transform(cyrillic)
+        if self.myPreciousPassword.max > 0:
+            self.myPreciousPassword.transform(self.myPreciousPassword.cut())
+        if self.checkUp.isChecked():
+            self.myPreciousPassword.up()
+            # print("после up", self.myPreciousPassword)
+        if self.myPreciousPassword.withDigits:
+            self.myPreciousPassword.add_digits()
+            # print("после цифр", self.myPreciousPassword)
+        if self.myPreciousPassword.withSymbols:
+            self.myPreciousPassword.add_symbols()
+        if not ' ' in self.myPreciousPassword.symbolsToUse:
+            self.myPreciousPassword.translit = ''.join(self.myPreciousPassword.translit.split(' '))
+
+        self.labelPassword.setText(str(self.myPreciousPassword))
 
 
 def main():
-    # при старте программы генерируется объект Password, который будет меняться после ходу изменения параметров и/или
-    # повторного нажатия на кнопку "Сгенерировать"
-    # вытягиваем пословицу, помещая в поле кириллицы
-    # транслитеруем, помещаем в поле транслита
-    # добавляем заглавные буквы, работаем с преобразованной строкой
-    # если есть ограничение на длину, обрезаем кириллический вариант и транслитеруем (добавляем заглавные буквы)
-    # если нужны цифры, добавляются, с учётом предыдущего параметра
-    # то же с символами
-    # при выводе пробелы "съедаются", впароле их быть не должно
-
     newPassword = Password()
-    cyrillic = newPassword.find_new()  # "Сгенерировать"
-    translit = newPassword.transform(cyrillic)
-    if len(newPassword) != 0:
-        translit = newPassword.transform(newPassword.cut())  # поле translit обновилось
-        uppered = newPassword.up()
-    uppered = newPassword.up()
-    withDig = input("С циферками? ")
-    if withDig == "да":
-        newPassword.withDigits = True
-    if newPassword.withDigits:
-        withDigits = newPassword.add_digits()
-    if newPassword.withSymbols:
-        pass
-    else:  # избавляемся от символов
-        splitted = translit.split(' ')
-        translit = ''.join(splitted)
-        p = re.compile("[^A-Za-z0-9]")
-        if len(p.findall(translit)):
-            found = set(p.findall(translit))
-            # print("i found", found)
-            for i in found:
-                splitted = translit.split(i)
-                translit = ''.join(splitted)
+    app = QtGui.QApplication(sys.argv)
+    win = Window(newPassword)
+    win.show()
+    app.exec()
 
-    print(cyrillic)
-    print(translit)
-    spl = uppered.split(' ')
-    print(''.join(spl))
-    if withDig == "да":
-        spl = withDigits.split(' ')
-        print(''.join(spl))
 
+# def main():
+#     # при старте программы генерируется объект Password, который будет меняться после
+#     # повторного нажатия на кнопку "Сгенерировать"
+#     # вытягиваем пословицу, помещая в поле кириллицы
+#     # транслитеруем, помещаем в поле транслита
+#     # добавляем заглавные буквы, работаем с преобразованной строкой
+#     # если есть ограничение на длину, обрезаем кириллический вариант и транслитеруем (добавляем заглавные буквы)
+#     # если нужны цифры, добавляются, с учётом предыдущего параметра
+#     # то же с символами
+#     # при выводе пробелы "съедаются", если их нет в списке допустимых символов
 
 if __name__ == '__main__':
     main()
